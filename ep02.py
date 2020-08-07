3643b7164cad3b4a0cd4d7868bd34bc76620b598
rafaelmspu2 <rafaelmspu2@gmail.com>
2020-08-07 00:17:51 -0300

    Update ep02.py

--- ep02.py
+++ ep02.py
@@ -1,39 +1,8 @@
-"""
-  AO PREENCHER ESSE CABEÇALHO COM O MEU NOME E O MEU NÚMERO USP,
-  DECLARO QUE SOU O ÚNICO AUTOR E RESPONSÁVEL POR ESSE PROGRAMA.
-  TODAS AS PARTES ORIGINAIS DESSE EXERCÍCIO PROGRAMA (EP) FORAM
-  DESENVOLVIDAS E IMPLEMENTADAS POR MIM SEGUINDO AS INSTRUÇÕES
-  DESSE EP E QUE PORTANTO NÃO CONSTITUEM DESONESTIDADE ACADÊMICA
-  OU PLÁGIO.
-  DECLARO TAMBÉM QUE SOU RESPONSÁVEL POR TODAS AS CÓPIAS
-  DESSE PROGRAMA E QUE EU NÃO DISTRIBUI OU FACILITEI A
-  SUA DISTRIBUIÇÃO. ESTOU CIENTE QUE OS CASOS DE PLÁGIO E
-  DESONESTIDADE ACADÊMICA SERÃO TRATADOS SEGUNDO OS CRITÉRIOS
-  DIVULGADOS NA PÁGINA DA DISCIPLINA.
-  ENTENDO QUE EPS SEM ASSINATURA NÃO SERÃO CORRIGIDOS E,
-  AINDA ASSIM, PODERÃO SER PUNIDOS POR DESONESTIDADE ACADÊMICA.
-
-  Nome : Rafael Agra de Castro Motta
-  NUSP : 11807192
-  Turma: 7
-  Prof.: André Fujita
-
-  Referências: Com exceção das rotinas fornecidas no enunciado
-  e em sala de aula, caso você tenha utilizado alguma refência,
-  liste-as abaixo para que o seu programa não seja considerado
-  plágio ou irregular.
-
-  Exemplo:
-  - O algoritmo Quicksort foi baseado em
-  http://www.ime.usp.br/~pf/algoritmos/aulas/quick.html
-"""
-
 
 # ======================================================================
-# FUNÇÕES OBRIGATÓRIAS
-# Implemente  neste bloco as funções obrigatórias do EP2.
-# NÃO modifique os nomes e parâmetros dessas funções.
+# FUNÇÕES Principais
 # ======================================================================
+
 def polinomioComRaiz(p, b) :
     """Devolve True se b é raiz do polinômio representado pela lista p,
        ou False no caso contrário.
@@ -275,9 +244,6 @@ def racionalToString(pn, r) :
 
 # ======================================================================
 # FUNÇÕES ADICIONAIS
-# Implemente neste bloco as funções adicionais às obrigatórias do EP2.
-# Duas funções desse tipo (a polinomioToString e a sig) foram
-# fornecidas no próprio enunciado do EP.
 # ======================================================================
 def polinomioToString(p) :
     """Devolve uma string que representa o polinômio em um formato
